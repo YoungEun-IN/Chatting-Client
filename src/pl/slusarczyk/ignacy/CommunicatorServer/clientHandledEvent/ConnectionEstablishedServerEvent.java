62faa24c95d5c7f2f2190b56f879514bc36ba362
YoungEun-IN <yyoungeunin@gmail.com>
2019-02-18 09:29:16 +0900

    isEstablished 삭제

--- src/pl/slusarczyk/ignacy/CommunicatorServer/clientHandledEvent/ConnectionEstablishedServerEvent.java
+++ src/pl/slusarczyk/ignacy/CommunicatorServer/clientHandledEvent/ConnectionEstablishedServerEvent.java
@@ -10,8 +10,6 @@ import pl.slusarczyk.ignacy.CommunicatorServer.model.data.UserIdData;
 public class ConnectionEstablishedServerEvent extends ClientHandledEvent implements Serializable
 {
 	 private static final long serialVersionUID = 1L;
-	/**연결 설정에 대한 표시*/
-	 private  final boolean isEstablished;
 	 /**감싸인 사용자 이름*/
 	 private final UserIdData userIDData;
 	 /**가입한 방의 이름*/
@@ -20,26 +18,15 @@ public class ConnectionEstablishedServerEvent extends ClientHandledEvent impleme
 	/**
 	 * 지정된 매개 변수를 기반으로 이벤트를 만드는 생성자
 	 * 
-	 * @param isEstablished 연결이 수락되었는지 여부
 	 * @param userIdData
 	 * @param roomName
 	 */
-	public ConnectionEstablishedServerEvent(final boolean isEstablished, final UserIdData userIdData, final String roomName)
+	public ConnectionEstablishedServerEvent(final UserIdData userIdData, final String roomName)
 	{
-		this.isEstablished = isEstablished;
 		this.userIDData = userIdData;
 		this.roomName = roomName;
 	}
 	
-	/**
-	 * 연결 수락에 대한 정보를 반환하는 메서드
-	 * 
-	 * @return isEstablished
-	 */
-	public boolean isEstablished()
-	{
-		return this.isEstablished;
-	}
 	
 	/**
 	 * userIDData를 반환
